3f9313845c9b02643b23ad3541ce6e4dee269c68
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2023-03-25 13:54:10 +0300

    removed unused import

--- bot/src/main/java/ru/tinkoff/edu/java/bot/exchandler/BotErrorControllerAdvice.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/exchandler/BotErrorControllerAdvice.java
@@ -1,7 +1,5 @@
 package ru.tinkoff.edu.java.bot.exchandler;
 
-import java.util.Arrays;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
