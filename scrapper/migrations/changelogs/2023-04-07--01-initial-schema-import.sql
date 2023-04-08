9bd3247f2b186cb8a1c32502ffad4c31c5d48e1b
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2023-04-08 22:06:21 +0300

    added if exist in creating table

--- scrapper/migrations/changelogs/2023-04-07--01-initial-schema-import.sql
+++ scrapper/migrations/changelogs/2023-04-07--01-initial-schema-import.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset Alexander-1703:add-link-table
-CREATE TABLE link
+CREATE TABLE IF NOT EXISTS link
 (
     id        BIGSERIAL PRIMARY KEY,
     link      VARCHAR(512) UNIQUE       NOT NULL,
@@ -9,13 +9,13 @@ CREATE TABLE link
 );
 
 --changeset Alexander-1703:add-chat-table
-CREATE TABLE chat
+CREATE TABLE IF NOT EXISTS chat
 (
     id BIGINT PRIMARY KEY
 );
 
 --changeset Alexander-1703:add-link_chat-table
-CREATE TABLE link_chat
+CREATE TABLE IF NOT EXISTS link_chat
 (
     linkId BIGINT,
     chatId BIGINT,
